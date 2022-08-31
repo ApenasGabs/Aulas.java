41ac81ea251dd292e6d1189d3cca2371938fd79d
Gabriel Rodrigues <Gabers357@gmail.com>
2022-08-31 20:09:39 -0300

    configuracoes iniciais

---
+++ Banco/src/br/com/banco/modelo/ContaBancaria.java
@@ -0,0 +1,46 @@
+package br.com.banco.modelo;
+
+public class ContaBancaria {
+	protected String  numconta;
+	protected String  cliente;
+	protected double saldo;
+	
+	
+	public String getNumconta() {
+		return numconta;
+	}
+
+
+	public void setNumconta(String numconta) {
+		this.numconta = numconta;
+	}
+
+
+	public String getCliente() {
+		return cliente;
+	}
+
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+
+	public static void main(String[] args) {
+		
+		
+		// TODO Kill my self
+
+	}
+
+}
